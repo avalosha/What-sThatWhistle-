43c306d2c708fa420eaa0155fcbbbd60e103870e
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-11-21 19:48:25 -0600

    Animate UIStackView subview layout.

---
+++ Project33/Project33/SubmitViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SubmitViewController.swift
+//  Project33
+//
+//  Created by Álvaro Ávalos Hernández on 21/11/20.
+//
+
+import UIKit
+
+class SubmitViewController: UIViewController {
+    
+    var genre: String!
+    var comments: String!
+    
+    var stackView: UIStackView!
+    var status: UILabel!
+    var spinner: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
