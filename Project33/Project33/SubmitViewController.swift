395356839d1430897d4655ffba1ca14d1329d215
Álvaro Ávalos Hernández <avalosha0805@gmail.com>
2020-11-21 22:23:51 -0600

    Writie to iCloud with CloudKit: CKRecord and CKAsset.

--- Project33/Project33/SubmitViewController.swift
+++ Project33/Project33/SubmitViewController.swift
@@ -63,7 +63,30 @@ class SubmitViewController: UIViewController {
     }
     
     func doSummission() {
+        //Establecer los datos a almacenar
+        let whistleRecord = CKRecord(recordType: "Whistles")
+        whistleRecord["genre"] = genre as CKRecordValue
+        whistleRecord["comments"] = comments as CKRecordValue
         
+        let audioURL = RecordWhistleViewController.getWhistleURL()
+        let whistleAsset = CKAsset(fileURL: audioURL)
+        whistleRecord["audio"] = whistleAsset
+        //Guardar en la BD de iCloud
+        CKContainer.default().publicCloudDatabase.save(whistleRecord) { [unowned self] record, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    self.status.text = "Error: \(error.localizedDescription)"
+                    self.spinner.stopAnimating()
+                } else {
+                    self.view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+                    self.status.text = "Done!"
+                    self.spinner.stopAnimating()
+                    
+                    ViewController.isDirty = true
+                }
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.doneTapped))
+            }
+        }
     }
     
     @objc func doneTapped() {
